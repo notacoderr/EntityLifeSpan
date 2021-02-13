2d9967acc90590c37de5c3981b835b82ae35ad73
notacoderr <38270548+notacoderr@users.noreply.github.com>
2021-02-13 05:22:32 +0400

    been a while
    NOTE:
    This update will not work with the previous version's timer.yml file, please delete it.
    
    New:
    - now with 3 modes of lifespan
    - much cleaner
    - projectile lifespan (those that got stuck on blocks)
    - item entity nametag (a neat looking one)

--- src/codeeeh/MainListener.php
+++ src/codeeeh/MainListener.php
@@ -5,68 +5,152 @@ namespace codeeeh;
 use pocketmine\{
 
 	plugin\PluginBase,
-		
+	
 	entity\object\ItemEntity,
-	entity\projectile\Arrow,
+	entity\projectile\Projectile,
 	
-	scheduler\Task,
 	utils\Config,
+	utils\TextFormat,
 	
-	event\Listener,
-	event\entity\ItemSpawnEvent
+	item\Durable,
 
-	};;
+	scheduler\Task,
+
+	event\Listener,
+	event\entity\ItemSpawnEvent,
+	event\entity\ProjectileHitBlockEvent
+
+};
 
 class MainListener extends PluginBase implements Listener
 {	
-	private $timer, $general;
+	private $mode = 0, $lifespan, $showname;
 	
 	function onEnable() : void
 	{
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		$this->saveResource('timer.yml');
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		
 		$cfg = new Config($this->getDataFolder() . "timer.yml", CONFIG::YAML);
-		$this->general = $cfg->get("general_mode");
-		if($this->general)
+
+		switch($cfg->get("mode"))
 		{
-			$this->timer = $cfg->get("general_timer");
-			$this->getServer()->getLogger()->info("General Mode activated");
-		} else {
-			$this->timer = $cfg->getNested("specific_timer");
-			$this->getServer()->getLogger()->info("Specific Mode activated");
+			case 'general':
+				$this->mode = 0;
+				$this->getServer()->getLogger()->info("General lifespan mode activated");
+			break;
+			case 'specific':
+				$this->mode = 1;
+				$this->getServer()->getLogger()->info("Specific lifespan mode activated");
+			break;
+			case 'mixed':
+				$this->mode = 2;
+				$this->getServer()->getLogger()->info("Mixed lifespan mode activated");
+			break;
+			default:
+				$this->mode = 0;
+				$this->getServer()->getLogger()->info("Unknown " . $cfg->get("mode") . " mode, using general instead.");
+		}
+		
+		$this->lifespan = $cfg->getNested('lifespan');
+		$this->showname = $cfg->getNested('showname');
+	}
+
+	/**
+	* @param ItemSpawnEvent $event
+	* priority HIGHEST
+	*/
+	public function onUnpextectedItemBirthSomewhere(ItemSpawnEvent $event)
+	{
+		if(($entity = $event->getEntity()) instanceof ItemEntity)
+		{
+			$itemname = $entity->getItem()->getVanillaName();
+			$itemnameL = strtolower($itemname);
+			switch($this->mode)
+			{
+				case 0:
+					$time = abs(20 * $this->lifespan['general']);
+				break;
+				
+				case 1:
+					if(array_key_exists($itemnameL, $this->lifespan['specific']))
+					{
+						$time = abs(20 * $this->lifespan['specific'][$itemnameL]);
+					} else {
+						return;
+					}
+				break;
+				
+				case 2:
+					if(array_key_exists($itemnameL, $this->lifespan['specific']))
+					{
+						$time = abs(20 * $this->lifespan['specific'][$itemnameL]);
+					} else {
+						$time = abs(20 * $this->lifespan['general']);
+					}
+				break;
+			}
+			
+			if($entity->isClosed()) return;
+			
+			if($this->showname)
+			{
+				$item = $entity->getItem();
+				if($item instanceof Durable)
+				{
+					$durability = ceil(
+						($item->getDamage() / $item->getMaxDurability()) * 100
+					);
+
+					switch(true)
+					{
+						case ($durability < 20): $color = TextFormat::GREEN;
+						break;
+						case (in_array($durability, range(21, 30))): $color = TextFormat::YELLOW;
+						break;
+						case (in_array($durability, range(31, 50))): $color = TextFormat::GOLD;
+						break;
+						case (in_array($durability, range(51, 80))): $color = TextFormat::RED;
+						break;
+						case ($durability > 81): $color = TextFormat::DARK_RED;
+						break;
+						default: $color = TextFormat::WHITE;
+					}
+					$nametag = "⌥ {$color}{$itemname}" . TextFormat::RESET;
+					$nametag .= "\n⌥ Enchantments: " . count($item->getEnchantments());
+				} else {
+					if($item->getCount() > 1) 
+					{
+						$nametag = "⌥ {$itemname}" . TextFormat::RESET;
+						$nametag .= "\n⌥ Count: " . $item->getCount();
+					} else {
+						$nametag = "⌥ {$itemname}";
+					}
+				}
+
+				$entity->setNameTag($nametag);
+				$entity->setNameTagAlwaysVisible();
+			}
+
+			$this->getScheduler()->scheduleDelayedTask(
+				new selfDestruction($entity), $time
+			);
 		}
 	}
 	
 	/**
-	*
-	* @param ItemEntity $event
+	* @param ProjectileHitBlockEvent $event
 	* priority HIGHEST
-	*
 	*/
-	public function onUnpextectedItemBirthSomewhere(ItemSpawnEvent $event)
+	function onProjectileStuck(ProjectileHitBlockEvent $event)
 	{
-		if(($ent = $event->getEntity()) instanceof ItemEntity)
+		if(($entity = $event->getEntity()) instanceof Projectile)
 		{
-			if($this->general)
-			{
-				$this
-				->getScheduler()
-				->scheduleDelayedTask
-				(
-					new selfDestruction($event->getEntity()), 20 * intval( $this->timer )
-				);
-			} else {
-				$itemName = strtolower( $ent->getItem()->getVanillaName() );
-				if( array_key_exists($itemName, $this->timer ) )
-				{
-					$this
-					->getScheduler()
-					->scheduleDelayedTask
-					(
-						new selfDestruction($event->getEntity()), 20 * intval( $this->timer[$itemName] )
-					);
-				}
-			}
+			if($entity->isClosed()) return;
+
+			$this->getScheduler()->scheduleDelayedTask(
+				new selfDestruction($entity), 20 * $this->lifespan['projectile']
+			);
 		}
 	}
 }
@@ -74,16 +158,14 @@ class MainListener extends PluginBase implements Listener
 class selfDestruction extends Task
 {
 	private $entity;
-	function __construct(ItemEntity $entity)
+
+	function __construct($entity)
 	{
 		$this->entity = $entity;
 	}
-	
+
 	public function onRun(int $i)
 	{
-		try
-		{ 
-			$this->entity->flagForDespawn();
-		} catch (\Exception $e) { }
+		if(!$this->entity->isClosed()) $this->entity->close();
 	}
 }
